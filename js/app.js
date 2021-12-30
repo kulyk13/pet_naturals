e28bd4b8957595b2fec09d13f2280cdbf489fc8f
kulyk13 <13dimon1313@gmail.com>
2021-12-30 20:45:10 +0200

    correcting styles & initializing slider

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
+//Slider
+const aboutSlider = new Swiper('.about-slider', {
+    loop: true,
+    breakpoints: {
+        320: {
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+            },
+        }
+    },
+})
+
 //Current date
 currentDate.textContent = new Date().toLocaleDateString();
