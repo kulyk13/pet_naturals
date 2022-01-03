9ccc59a363a3d8d167c6e98e641cd3452577729c
kulyk13 <13dimon1313@gmail.com>
2022-01-03 13:58:50 +0200

    stylizing header & hero

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+//Timer
+let endDate = + new Date() + 7200100;
+
+let timer = setInterval(function() {
+    let now = new Date().getTime();
+    let t = endDate - now;
+    if (t >= 0) {
+        let hrs = Math.floor((t % (1000 * 60 * 60 * 60)) / (1000 * 60 * 60));
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 60)) / 1000);
+        document.getElementById("timerHours").innerHTML = ("0" + hrs).slice(-2);
+        document.getElementById("timerMinutes").innerHTML = ("0" + mins).slice(-2);
+        document.getElementById("timerSeconds").innerHTML = ("0" + secs).slice(-2);  
+    } else {
+        document.getElementById("timer").innerHTML = "Times Up!"
+    }
+}, 1000)
+
 //Slider
 const aboutSlider = new Swiper('.about-slider', {
     loop: true,
